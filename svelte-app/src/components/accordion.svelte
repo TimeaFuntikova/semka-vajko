cf0b8515fdae2c5132fdcad4068bff9f4298acc0
timea <timea.funtik@softacus.com>
2023-10-24 18:16:38 +0200

    acc - component - for styling dashboard - from lib

---
+++ svelte-app/src/components/accordion.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Accordion, AccordionItem } from "carbon-components-svelte";
+</script>
+
+<Accordion>
+    <AccordionItem title="Course Title 1">
+        <p>
+            Course description for Course Title 1. This course covers...
+        </p>
+    </AccordionItem>
+
+    <AccordionItem title="Course Title 2">
+        <p>
+            Course description for Course Title 2. This course covers...
+        </p>
+    </AccordionItem>
+    <!-- Add more AccordionItem elements for additional courses -->
+</Accordion>
