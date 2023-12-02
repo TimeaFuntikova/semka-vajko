982e5b97890cc98b548d32813a1adb3068c8b0a7
timea <timea.funtik@softacus.com>
2023-12-02 17:55:19 +0100

    initial commit

---
+++ semestralProjectVajko/semestralProjectVajko/src/main/java/com/api/semestralProjectVajko/SemestralProjectVajkoApplication.java
@@ -0,0 +1,13 @@
+package com.api.semestralProjectVajko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemestralProjectVajkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemestralProjectVajkoApplication.class, args);
+	}
+
+}
---
+++ semestralProjectVajko/semestralProjectVajko/src/test/java/com/api/semestralProjectVajko/SemestralProjectVajkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.semestralProjectVajko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemestralProjectVajkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
