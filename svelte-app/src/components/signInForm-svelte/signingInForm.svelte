023524500e935a67c9cb4fbf29ffe6acde8331fc
timea <timea.funtik@softacus.com>
2023-12-03 23:19:35 +0100

    updated on signingInForm

---
+++ svelte-app/src/components/signInForm-svelte/signingInForm.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import {
+        FluidForm,
+        TextInput,
+        PasswordInput,
+    } from "carbon-components-svelte";
+    import {isPasswordValid, passwordToStore, usernameToStore} from "@/storage/form.storage";
+    import {AppModel} from "@/types/AppModel";
+
+    let password: string = "";
+    let username: string = "";
+    isPasswordValid.set(false);
+
+    function handleInput(password: string): boolean {
+        if (AppModel.service.handler.isPasswordValid(password)) {
+            isPasswordValid.set(true);
+            AppModel.service.formDataHandler.storeUsername(username);
+            AppModel.service.formDataHandler.storePassword(password);
+            return true;
+        }
+    }
+</script>
+
+<FluidForm>
+    <TextInput
+            bind:value={username}
+            labelText="User name"
+            placeholder="Enter user name..."
+            required />
+    <PasswordInput
+            bind:value={password}
+            invalid={!handleInput(password)}
+            invalidText="Your password must be at least 6 characters as well as contain at least one uppercase, one special character and one number."
+            required
+            type="password"
+            labelText="Password"
+            placeholder="Enter password..."
+    />
+</FluidForm>
