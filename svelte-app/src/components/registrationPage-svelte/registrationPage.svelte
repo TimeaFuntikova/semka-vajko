904e52830e466acba2f9e650f27342b23e24f095
timea <timea.funtik@softacus.com>
2023-12-05 00:00:37 +0100

    created registration page

---
+++ svelte-app/src/components/registrationPage-svelte/registrationPage.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Form from "../signInForm-svelte/signingInForm.svelte";
+    import ButtonRegister from "../registrationPage-svelte/registrationButton.svelte";
+
+</script>
+<link rel="stylesheet" href="../styles.css">
+<header>
+    <h1>Registration</h1>
+</header>
+
+<main>
+    <Form/>
+    <ButtonRegister/>
+</main>
