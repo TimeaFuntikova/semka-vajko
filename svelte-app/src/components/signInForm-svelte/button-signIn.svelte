e17e2898d4f7b24bf71d5176938d5a7f3167d409
timea <timea.funtik@softacus.com>
2023-12-05 00:01:07 +0100

    updated on reactivity in signIn button

--- svelte-app/src/components/signInForm-svelte/button-signIn.svelte
+++ svelte-app/src/components/signInForm-svelte/button-signIn.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
     import { ButtonSet, Button } from "carbon-components-svelte";
     import Login from "carbon-icons-svelte/lib/Login.svelte";
-    import {AppModel} from "@/types/AppModel";
+    import { AppModel } from "@/types/AppModel";
+    import RegistrationPage from "../registrationPage-svelte/registrationPage.svelte";
+
+    let isRegistrationVisible = false;
 
     function handleClick(e) {
-        AppModel.service.handler.handleSubmit();
+        //if(AppModel.service.handler.validInputs()) AppModel.service.handler.fetchFromServer();
     }
+
+    function handleRegistrationClick() {
+        isRegistrationVisible = !isRegistrationVisible;
+    }
+
+    const pages = [
+        { name: "Registration", component: RegistrationPage },
+    ];
 </script>
+
+{#if isRegistrationVisible}
+    <svelte:component this={pages[0].component} />
+{/if}
 
 <ButtonSet stacked>
     <Button
             on:click={e => handleClick(e)}
             icon={Login}>Log in
     </Button>
-    <Button kind="ghost">Sign up</Button>
+    <Button kind="ghost"
+            on:click={handleRegistrationClick}
+    >{isRegistrationVisible ? 'Hide' : 'Sign up'}</Button>
 </ButtonSet>
