abe6c288062f01fd23b0d035ae5e0b3f3f93322c
timea <timea.funtik@softacus.com>
2023-12-06 15:15:50 +0100

    profile page created

---
+++ svelte-app/src/components/pages/profile.svelte
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="../styles.css">
+<header>
+    <h1>Welcome to your profile page.</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="#">Courses</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Login</a></li>
+    </ul>
+</nav>
+<main>
+    <div class="course-card">
+        <h3>PROFILE</h3>
+        <p>Short description goes here.</p>
+        <p>Add or change role.</p>
+        <p>Edit username</p>
+        <p>Edit password</p>
+
+    </div>
+</main>
