3dcdb3be65f287b6038322b0208189708c964b71
timea <timea.funtik@softacus.com>
2023-12-07 00:32:35 +0100

    updated sending requests and clients logic

--- svelte-app/src/service/RequestsHandler.ts
+++ svelte-app/src/service/RequestsHandler.ts
@@ -1,7 +1,19 @@
 import { AppModel } from "@/types/AppModel";
 import { UserRegistrationRequest } from "@/types/UserRegistrationRequest";
+import { Response } from "express";
+import { User } from "@/types/User";
+import { isLoggedIn } from "@/storage/form.storage";
 
 export class RequestsHandler {
+  allUsersObtained: string = "";
+
+  async setAllUsers(users: string): Promise<void> {
+    this.allUsersObtained = users;
+  }
+
+  getAllUsers(): string {
+    return this.allUsersObtained;
+  }
   isPasswordValid(password: string): boolean {
     return this.check(password);
   }
@@ -17,10 +29,41 @@ export class RequestsHandler {
 
   validInputs(): boolean {
     const username: string = AppModel.service.formDataHandler.getUsername();
-    console.log("username: ", username);
     const password: string = AppModel.service.formDataHandler.getPassword();
-    console.log("password: ", password);
     return username != "" && password != "";
+  }
+
+  private async parseUserFromString(obtainedUsers: string): Promise<User[]> {
+    try {
+      return JSON.parse(obtainedUsers);
+    } catch (error) {
+      console.error("Error occurred while parsing user JSON file.", error);
+      return [];
+    }
+  }
+  async isRegistered(): Promise<boolean> {
+    const obtainedUsers: string = AppModel.service.handler.getAllUsers();
+    const userObjects: User[] = await this.parseUserFromString(obtainedUsers);
+
+    if (userObjects) {
+      const username: string = AppModel.service.formDataHandler.getUsername();
+      for (const user of userObjects) {
+        if (user.username === username) return true;
+      }
+    }
+    return false;
+  }
+
+  async loggedIn(): Promise<void> {
+    await this.sendVerifyRequest();
+    isLoggedIn.set(true);
+    console.log("User has been logged in.");
+    //TODO:  redirect to profile page.
+  }
+
+  unregisteredUser(): void {
+    //TODO: create notification and provide registration form.(pehraps via reactive storage value).
+    console.log("User is NOT registered.");
   }
 
   /**
@@ -28,8 +71,8 @@ export class RequestsHandler {
    * Sends request to server to ensure, if the user already exists.
    * If he does not, a new user in the database is created and returned for the user as notification.
    */
-  async sendRegistrationRequest() {
-    const requestData: UserRegistrationRequest = {
+  async sendRegistrationRequest(): Promise<void> {
+    const requestData: { password: string; name: string } = {
       name: AppModel.service.formDataHandler.getUsername(),
       password: AppModel.service.formDataHandler.getPassword(),
     };
@@ -41,45 +84,120 @@ export class RequestsHandler {
       },
       body: JSON.stringify(requestData),
     })
-      .then((response: Response) => {
+      .then(async (response: Response): Promise<any> => {
         if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
+          try {
+            const errorResponse = await response.json();
+            throw new Error(
+              `HTTP error! Status: ${response.status}, Message: ${errorResponse.message}`,
+            );
+          } catch (error) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
         }
-        const responseFromServer: Promise<string> = response.text();
-        console.log(responseFromServer);
-        return responseFromServer;
+        return response.json();
       })
-      .then((data: string): void => {
+      .then((data: any): void => {
         console.log("Success: ", data);
       })
-      .catch((error): void => {
-        console.error("Error: ", error);
+      .catch((error: Error): void => {
+        console.error("Error: ", error.message);
       });
   }
+
   /**
-   * This request should decide whether a user already exists in the database.
-   * If not-> should redirect to register formular and THEN should create the user in db
-   * If already does -> should redirect to login landing page.
+   * Checks if the login is successful.
    */
-  //TODO: redirecting to sites depending on the login status.
-  fetchFromServer(): void {
-    console.log("Data retrieved is valid. Sending request.");
-    fetch("/api/isRegistered", {
+  async sendVerifyRequest(): Promise<void> {
+    const requestData: UserRegistrationRequest = {
+      name: AppModel.service.formDataHandler.getUsername(),
+      password: AppModel.service.formDataHandler.getPassword(),
+      newPasswordDemand: "",
+      newNameDemand: "",
+    };
+
+    fetch("http://localhost:8080/api/verify", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        namehow: AppModel.service.formDataHandler.getUsername(),
-        password: AppModel.service.formDataHandler.getPassword(),
-      }),
+      body: JSON.stringify(requestData),
     })
-      .then((response: Response) => response.text())
-      .then((data: string): void => {
+      .then(async (response: Response): Promise<any> => {
+        if (!response.ok) {
+          try {
+            const errorResponse = await response.json();
+          } catch (error) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+        }
+        return response.json();
+      })
+      .then((data: any): void => {
         console.log("Success: ", data);
       })
-      .catch((error) => {
-        console.log("Error: ", error);
+      .catch((error: Error): void => {
+        console.error("Error: ", error.message);
+      });
+  }
+
+  /**
+   * This request should decide whether a user already exists in the database.
+   * If not-> should redirect to register form and THEN should create the user in db
+   * If already does -> should redirect to login landing page.
+   */
+  //TODO: redirecting to sites depending on the login status.
+  async sendRequestGetAllUsers(): Promise<string> {
+    try {
+      const response: Response = await fetch(
+        "http://localhost:8080/api/getAllUsers",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      return await response.text();
+    } catch (error) {
+      console.error("Error: ", error);
+      throw error;
+    }
+  }
+
+  async sendUpdateRequest(): Promise<void> {
+    const requestData: UserRegistrationRequest = {
+      name: AppModel.service.formDataHandler.getUsername(),
+      password: AppModel.service.formDataHandler.getPassword(),
+      newNameDemand: AppModel.service.formDataHandler.getNewUsername(),
+      newPasswordDemand: AppModel.service.formDataHandler.getNewPassword(),
+    };
+
+    console.log(requestData);
+
+    fetch("http://localhost:8080/api/updateUserData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    })
+      .then(async (response: Response): Promise<any> => {
+        if (!response.ok) {
+          try {
+            const errorResponse = await response.text();
+          } catch (error) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+        }
+        return response.text();
+      })
+      .then((data: any): void => {
+        console.log("Success: ", data);
+      })
+      .catch((error: Error): void => {
+        console.error("Error: ", error.message);
       });
   }
 }
