c8a18849e54946f89ba4ec39c3be85ef132ab8d3
timea <timea.funtik@softacus.com>
2023-12-07 00:31:58 +0100

    update functionality

---
+++ svelte-app/src/components/changingData/updateDataButton.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Button } from "carbon-components-svelte";
+    import {AppModel} from "@/types/AppModel.js";
+
+    function handleUpdateClick(e) {
+        AppModel.service.handler.sendUpdateRequest();
+    }
+</script>
+
+<Button
+on:click={e => handleUpdateClick(e)}>Update Data</Button>
---
+++ svelte-app/src/components/changingData/updateForm.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import {
+        FluidForm,
+        TextInput,
+        PasswordInput,
+    } from "carbon-components-svelte";
+    import {isPasswordValid} from "@/storage/form.storage";
+    import {AppModel} from "@/types/AppModel";
+
+    let newPassword: string = "";
+    let newUsername: string = "";
+    isPasswordValid.set(false);
+
+    function handleInput(password: string): boolean {
+        if (AppModel.service.handler.isPasswordValid(password)) {
+            isPasswordValid.set(true);
+            AppModel.service.formDataHandler.setNewUsername(newUsername);
+            AppModel.service.formDataHandler.setNewPassword(newPassword);
+            return true;
+        }
+    }
+</script>
+
+<FluidForm>
+    <TextInput
+            bind:value={newUsername}
+            labelText="User name"
+            placeholder="Enter user name..."
+            required />
+    <PasswordInput
+            bind:value={newPassword}
+            invalid={!handleInput(newPassword)}
+            invalidText="Your password must be at least 6 characters as well as contain at least one uppercase, one special character and one number."
+            required
+            type="password"
+            labelText="Password"
+            placeholder="Enter password..."
+    />
+</FluidForm>
