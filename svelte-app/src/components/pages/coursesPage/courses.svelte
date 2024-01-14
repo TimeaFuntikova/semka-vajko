48ac339dfbfe61160c0dadd268c8c6bbad43b2df
timea <timea.funtik@softacus.com>
2024-01-14 01:09:08 +0100

    updated on available courses page

--- svelte-app/src/components/pages/coursesPage/courses.svelte
+++ svelte-app/src/components/pages/coursesPage/courses.svelte
@@ -1,5 +1,29 @@
-<div class="course-card">
-    <h3>LIST OF AVAILABLE COURSES...</h3>
-    <p>Short description goes here.</p>
-    <a href="#">Learn More</a>
+<script lang="ts">
+    import Pagination from "../../pagination.svelte";
+    import EnglishCourse from "../../englishPic.svelte";
+
+</script>
+
+<h3 style="text-align: center">Here you can browse the available courses.</h3>
+<div class="row">
+    <div class="main">
+        <div class="course-card">
+            <h3>English Course</h3>
+            <p>Course description goes here. This course covers...</p>
+            <a href="#">Learn More</a>
+        </div>
+        <div class="course-card">
+            <EnglishCourse/>
+            <h3>German Course</h3>
+            <p>Course description goes here. This course covers...</p>
+            <a href="#">Learn More</a>
+        </div>
+        <div class="course-card">
+            <EnglishCourse/>
+            <h3>VAII Course</h3>
+            <p>Course description goes here. This course covers...</p>
+            <a href="#">Learn More</a>
+        </div>
+        <Pagination/>
+    </div>
 </div>
